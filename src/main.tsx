d535ee97519bf2c64228618d6f1e9ac3e232926f
Eric <eriicsilva28@gmail.com.br>
2024-08-04 14:03:29 -0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+// import './Interview-questions/6-custom-filter/styles/styles-custom.css'
 import './index.css'
 // import './Interview-questions/5-context-and-performance/styles/context-global.css'
-import { Providers } from './providers/Providers.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    {/* <Providers> */}
+    <App />
+    {/* </Providers> */}
   </React.StrictMode>
 )
