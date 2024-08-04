3527760dfcde49379af99d49a4b96d0e40f14819
Eric <eriicsilva28@gmail.com.br>
2024-08-04 10:03:29 -0300

    feat: implemente promise.all solution to fetch api

---
+++ src/Interview-questions/4-data-fetching-on-the-client/2-promise-all/Sidebar.tsx
@@ -0,0 +1,16 @@
+import { SidebarType } from '../shared/api/data'
+import '../shared/styles/styles.css'
+
+const Sidebar = ({ data }: { data: SidebarType[] }) => {
+  return (
+    <div className='sidebar sidebar-base'>
+      <ul>
+        {data.map(({ name, id }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
