a8dbb08ffd80e353fd64314c441e497182d3adc1
Eric <eriicsilva28@gmail.com.br>
2024-08-03 21:14:05 -0300

    feat: apply style in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// import './Interview-questions/5-context-and-performance/styles/context-global.css'
 import { Providers } from './providers/Providers.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
