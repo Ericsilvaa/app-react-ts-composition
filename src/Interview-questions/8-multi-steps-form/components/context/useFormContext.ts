3c5b5f4a43ad70ee4013664654c869399ea5c2de
Eric <eriicsilva28@gmail.com.br>
2024-08-05 19:53:36 -0300

    troca

--- src/Interview-questions/8-multi-steps-form/components/context/useFormContext.ts
+++ src/Interview-questions/8-multi-steps-form/components/context/useFormContext.ts
@@ -5,7 +5,7 @@ export const useFormContext = () => {
   const context = React.useContext(UserContext)
 
   if (!context) {
-    throw new Error('Tu ta mexendo onde não deve, meu fi!!!')
+    throw new Error('Tu ta mexendo onde não deve, meu fi!!')
   }
   return context
 }
