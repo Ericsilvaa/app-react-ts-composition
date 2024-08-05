a985c762e0edf79acac27c1898f4a33dbcbd44fd
Eric <eriicsilva28@gmail.com.br>
2024-08-05 20:35:43 -0300

    feat: multi step e types

---
+++ src/Interview-questions/8-multi-steps-form/components/form/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import { useFormContext } from '../context/useFormContext'
+
+const ProgressBar = () => {
+  const { currentStep } = useFormContext()
+
+  return (
+    <div>
+      <h1>Step {currentStep} of 5</h1>
+      <div>
+        {[1, 2, 3, 4, 5].map((step) => (
+          <div
+            key={step}
+            style={{
+              width: '20px',
+              height: '20px',
+              borderRadius: '50%',
+              backgroundColor: step <= currentStep ? 'green' : 'gray'
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
