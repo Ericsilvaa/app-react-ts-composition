285db4ed395ffce0ca8a3244037fc9bad94ccaaf
Eric <eriicsilva28@gmail.com.br>
2024-08-06 01:22:55 -0300

    refactor: aplicando TextInput

--- src/Interview-questions/8-multi-steps-form/components/form/steps/PersonalInformation.tsx
+++ src/Interview-questions/8-multi-steps-form/components/form/steps/PersonalInformation.tsx
@@ -1,5 +1,6 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useFormContext } from '../../../context/useFormContext'
+import TextInput from '../../input/TextInput'
 import { DataForm } from '../../types/TForm'
 
 type FormData = DataForm[`personal_information`]
@@ -8,6 +9,7 @@ const PersonalInformation = () => {
   const { setFieldValue, nextStep } = useFormContext()
 
   const { register, handleSubmit } = useForm<FormData>()
+
   const onSubmit: SubmitHandler<FormData> = (formData) => {
     console.log('data', formData)
     setFieldValue('personal_information', formData)
@@ -20,56 +22,22 @@ const PersonalInformation = () => {
       onSubmit={handleSubmit(onSubmit)}
       className='max-w-md mx-auto p-4 bg-white shadow-sm rounded-md'
     >
-      <div className='mb-4'>
-        <label
-          htmlFor='fullName'
-          className='block text-sm font-medium text-gray-700'
-        >
-          Full Name
-        </label>
-        <input
-          type='text'
-          id='fullName'
-          className='mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-          {...register('fullName')}
-        />
-      </div>
-      <div className='mb-4'>
-        <label
-          htmlFor='email'
-          className='block text-sm font-medium text-gray-700'
-        >
-          Email
-        </label>
-        <input
-          type='email'
-          id='email'
-          className='mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-          {...register('email')}
-        />
-      </div>
-      <div className='mb-4'>
-        <label
-          htmlFor='birthDate'
-          className='block text-sm font-medium text-gray-700'
-        >
-          Birth Date
-        </label>
-        <input
-          type='date'
-          id='birthDate'
-          className='mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-          {...register('birthDate')}
-        />
-      </div>
-      <div className='mb-4'>
-        <label
+      <TextInput id='fullName' label='Full Name' {...register('fullName')} />
+      <TextInput id='email' label='Email' {...register('email')} />
+      <TextInput
+        id='birthDate'
+        type='date'
+        label='Birth Date'
+        {...register('birthDate')}
+      />
+      {/* <div className='mb-4'> */}
+      {/* <label
           htmlFor='gender'
           className='block text-sm font-medium text-gray-700'
         >
           Gender
-        </label>
-        {/* <select
+        </label> */}
+      {/* <select
           id='gender'
           name='gender'
           value={data.personal_information?.gender}
@@ -80,7 +48,7 @@ const PersonalInformation = () => {
           <option value='female'>Female</option>
           <option value='other'>Other</option>
         </select> */}
-      </div>
+      {/* </div> */}
       <button
         type='submit'
         className='w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
